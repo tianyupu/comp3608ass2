8fe49138a53cbc55df953fd8c7981370601002cf
Tianyu Pu <divine.infinity@gmail.com>
2012-05-19 23:23:34 +1000

    data preprocessed successfully, weighting data needs checking for correctness

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 if __name__ == '__main__':
   from preprocessor import *
   directory = sys.argv[1]
-  if directory:
-  	subj_corp, body_corp = preprocess(directory)
-  else:
-    sys.stderr.write('main.py: no directory specified, exiting\n')
-# select
-# normalise
+  subj_corp, body_corp = preprocess(directory)
+  nfeatures = int(sys.argv[2])
+  subj_sel = df_select(subj_corp, nfeatures)
+  body_sel = df_select(body_corp, nfeatures)
+  write_dataset(subj_sel, directory, 'subject.csv')
+  write_dataset(body_sel, directory, 'body.csv')
 # NB and cross-validation
--- preprocessor.py
+++ preprocessor.py
@@ -3,6 +3,7 @@
 import re
 import os, os.path, sys
 import gzip
+from math import log10, sqrt
 
 class Corpus(object):
   """A class to represent a corpus of text.
@@ -34,6 +35,9 @@ class Corpus(object):
   def get_words(self):
     """Get all the words for this corpus."""
     return self.words.get_words()
+  def get_word(self, word):
+    """Return the word object corresponding to the specified word."""
+    return self.words.get_word(word)
   def get_df(self, word):
     """Get the document frequency (DF) for a particular word.
     
@@ -115,7 +119,7 @@ class WordList(object):
       word_obj = Word(word, fname)
       self.words[word] = word_obj
   def get_words(self):
-    return self.words.keys()
+    return self.words.values()
   def get_word(self, text):
     if text in self.words:
       return self.words[text]
@@ -133,16 +137,71 @@ def preprocess(source_dir):
     content = src_f.read()
     content_lines = content.split('\n', 1)
     subj, body = content_lines[0].strip(), content_lines[1].strip()
+    subj = subj.lstrip('Subject: ')
     subj_corp.add(subj, f)
     body_corp.add(body, f)
+    src_f.close()
   return subj_corp, body_corp
 
 def df_select(corpus, max_no):
+  df_vals = {}
   selected = []
-  lowest = 0
-  highest = 0
+  count = 0
   for word in corpus.get_words():
-    pass
+    df = word.get_df()
+    if df in df_vals:
+      df_vals[df].append(word)
+    else:
+      df_vals[df] = [word]
+  for df_val in sorted(df_vals, reverse=True):
+    while df_vals[df_val]:
+      if count < max_no:
+        word = df_vals[df_val].pop()
+        selected.append(word)
+        count += 1
+      else:
+        return selected
+#    df_vals.append((corpus.get_df(word), corpus.get_word(word)))
+#  df_vals.sort(cmp=lambda x,y:cmp(x[0],y[0]), reverse=True)
+#  return df_vals[:max_no]
 
-def tf_idf():
-  pass
+def write_dataset(df_list, src_dir, sav_name):
+  nfeatures = len(df_list)
+  header_list = ['f%d' % x for x in xrange(1,nfeatures+1)]
+  header_list.append('class')
+  header_str = ','.join(header_list) + '\n'
+
+  src_files = os.listdir(src_dir)
+  collection_size = len(src_files)
+  weights = []
+  for f in src_files:
+    row = []
+    for word_obj in df_list:
+      row.append(str(cos_norm(word_obj, f, df_list, collection_size)))
+    if 'spmsg' in f:
+      row.append('spam')
+    else:
+      row.append('nonspam')
+    row_str = ','.join(row)
+    weights.append(row_str)
+  weight_str = '\n'.join(weights)
+
+  out_f = open(sav_name, 'w')
+  out_f.write(header_str+weight_str)
+  out_f.close()
+
+def tf_idf(word, fname, collection_size):
+  # tfidf(t_k, d_j) = #(t_k, d_j) * log(|Tr| / #Tr(t_k))
+  tf_idf = word.get_freq(fname) * log10(float(collection_size)/word.get_df())
+  return tf_idf
+
+def cos_norm(word, fname, words, collection_size):
+  numerator = tf_idf(word, fname, collection_size)
+  denom = 0.0
+  for word in words:
+    denom += tf_idf(word, fname, collection_size) ** 2
+  if denom == 0:
+    weight = 0
+  else:
+    weight = numerator / sqrt(denom)
+  return weight
