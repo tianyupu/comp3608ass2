4b60289e46a1b2458b1d3271015aca40525affa9
Tianyu Pu <divine.infinity@gmail.com>
2012-05-26 12:18:00 +1000

    moved .csv files into a results folder
    cleans up the root repository directory

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
 if __name__ == '__main__':
+  import os.path
   from preprocessor import *
   from classifier import *
+
+  RESULT_DIR = 'results'
 
   SUBJ_FNAME = 'subject.csv'
   BODY_FNAME = 'body.csv'
@@ -12,10 +15,10 @@ if __name__ == '__main__':
   nfeatures = int(sys.argv[2]) # no. features to select
   subj_sel = feature_select(subj_corp, 'df', nfeatures)
   body_sel = feature_select(body_corp, 'df', nfeatures)
-  #write_dataset(subj_sel, directory, SUBJ_FNAME)
-  #write_dataset(body_sel, directory, BODY_FNAME)
+  #write_dataset(subj_sel, directory, os.path.join(RESULT_DIR, SUBJ_FNAME))
+  #write_dataset(body_sel, directory, os.path.join(BODY_FNAME))
 
-  subj_classifier = Classifier(SUBJ_FNAME)
-  body_classifier = Classifier(BODY_FNAME)
+  subj_classifier = Classifier(os.path.join(RESULT_DIR, SUBJ_FNAME))
+  body_classifier = Classifier(os.path.join(RESULT_DIR, BODY_FNAME))
   subj_classifier.train()
   body_classifier.train()
