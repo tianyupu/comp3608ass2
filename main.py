e97d4d4a8d6c8c68d3954d7771bf63d3eab62cce
dongjie <donaldzhang@live.com>
2012-05-28 07:18:37 +1000

    still need to finish extension section in report, added extension stuff to main.py

--- main.py
+++ main.py
@@ -4,21 +4,45 @@ if __name__ == '__main__':
   import os.path
   from preprocessor import *
   from classifier import *
+  from validation import *
 
   RESULT_DIR = 'results'
 
   SUBJ_FNAME = 'subject.csv'
   BODY_FNAME = 'body.csv'
 
-  directory = sys.argv[1]
-  subj_corp, body_corp = preprocess(directory, False)
-  nfeatures = int(sys.argv[2]) # no. features to select
-  subj_sel = feature_select(subj_corp, 'df', nfeatures)
-  body_sel = feature_select(body_corp, 'df', nfeatures)
-  #write_dataset(subj_sel, directory, os.path.join(RESULT_DIR, SUBJ_FNAME))
-  #write_dataset(body_sel, directory, os.path.join(BODY_FNAME))
+#commented out region creates weighted feature datasets
+##  directory = sys.argv[1]
+##  subj_corp, body_corp = preprocess(directory, False)
+##  stem_subj_corp, stem_body_corp = preprocess(directory, True)
+##  nfeatures = int(sys.argv[2]) # no. features to select
+##  subj_sel = feature_select(subj_corp, 'df', nfeatures)
+##  body_sel = feature_select(body_corp, 'df', nfeatures)
+##  stem_subj_sel = feature_select(stem_subj_corp, 'df', nfeatures)
+##  stem_body_sel = feature_select(stem_body_corp, 'df', nfeatures)
+##  cpd_subj_sel = feature_select(subj_corp, 'cpd', nfeatures)
+##  cpd_body_sel = feature_select(body_corp, 'cpd', nfeatures)
+##  cpd_stem_subj_sel = feature_select(stem_subj_corp, 'cpd', nfeatures)
+##  cpd_stem_body_sel = feature_select(stem_body_corp, 'cpd', nfeatures)
+##  
+##  write_dataset(subj_sel, directory, os.path.join(RESULT_DIR, SUBJ_FNAME))
+##  write_dataset(body_sel, directory, os.path.join(RESULT_DIR, BODY_FNAME))
+##
+##  write_dataset(stem_subj_sel, directory, os.path.join(RESULT_DIR, "stem_"+SUBJ_FNAME))
+##  write_dataset(stem_body_sel, directory, os.path.join(RESULT_DIR, "stem_"+BODY_FNAME))
+##  
+##  write_dataset(cpd_subj_sel, directory, os.path.join(RESULT_DIR, "cpd_"+SUBJ_FNAME))
+##  write_dataset(cpd_body_sel, directory, os.path.join(RESULT_DIR, "cpd_"+BODY_FNAME))
+##
+##  write_dataset(cpd_stem_subj_sel, directory, os.path.join(RESULT_DIR, "cpd_stem_"+SUBJ_FNAME))
+##  write_dataset(cpd_stem_body_sel, directory, os.path.join(RESULT_DIR, "cpd_stem_"+BODY_FNAME))
 
-  subj_classifier = Classifier(os.path.join(RESULT_DIR, SUBJ_FNAME))
-  body_classifier = Classifier(os.path.join(RESULT_DIR, BODY_FNAME))
-  subj_classifier.train()
-  body_classifier.train()
+  #cross validation
+  cross_validation(SUBJ_FNAME, 10)
+  cross_validation(BODY_FNAME, 10)
+  cross_validation("stem_"+SUBJ_FNAME, 10)
+  cross_validation("stem_"+BODY_FNAME, 10)
+  cross_validation("cpd_"+SUBJ_FNAME, 10)
+  cross_validation("cpd_"+BODY_FNAME, 10)
+  cross_validation("cpd_stem"+SUBJ_FNAME, 10)
+  cross_validation("cpd_stem"+BODY_FNAME, 10)
--- validation.py
+++ validation.py
@@ -9,78 +9,86 @@ from preprocessor import *
 from classifier import *
 
 def cross_validation(csv_fname, folds):
-    """Runs n-fold validation on the examples (each example is a weighted feature
-    list) in a csv file. folds = number of folds"""
-    
-    # read csv file and extract lines
-    f_handle = open("results/"+csv_fname, 'rU')
-    lines = [line.strip() for line in f_handle.readlines()]
-    header_str = lines[0]
-    f_handle.close()
-    # create stratified subsets. don't use the first line. It is the header
-    subsets = get_validationsets(lines[1:], folds)
-
-    # list to store the classifier accuracy for each fold
-    results = []
-    for index in range(len(subsets)):
-        
-        validation_set = subsets[index]
-        training_set = subsets[:index] + subsets[index + 1:]
-        # flatten the training set into a list of stringss
-        training_set = [item for subset in training_set for item in subset]
-        
-        # output a csv file for each validation fold
-        test_fname = "validation/test_"+str(index)+csv_fname
-        out_f = open(test_fname, 'w')
-        out_f.write(header_str+"\n"+ string.join([(line+"\n") for line in training_set]))
-        out_f.close()
+  """Runs n-fold validation on the examples (each example is a weighted feature
+  list) in a csv file. folds = number of folds"""
   
-        # make the classifier
-        classifier = Classifier(test_fname)
-        classifier.train()
-        # test each example in validation set
-        correct = 0
-        incorrect = 0
-        for line in validation_set:
-            if classifier.test_example(line.strip()):
-                correct += 1
-        accuracy = float(correct)*100/len(validation_set)
-        print "accuracy: " + str(accuracy) +"%"
-        results.append(accuracy)
-    return sum(results)/folds
-                
-def get_validationsets(lines, folds):
-    """Returns a randomly stratified n-fold sample of a list of lines"""
-    spam_lines = []
-    nonspam_lines = []
-    for line in lines:
-        if 'nonspam' in line:
-            nonspam_lines.append(line)
-        else:
-            spam_lines.append(line)
-    spam_partitions = random_partition(spam_lines, folds)
-    nonspam_partitions = random_partition(nonspam_lines, folds)
-    validation_sets = []
-    for i in xrange(folds):
-        validation_sets.append([])
-        validation_sets[i].extend(spam_partitions[i])
-        validation_sets[i].extend(nonspam_partitions[folds - i - 1])
-        random.shuffle(validation_sets[i])
-    return validation_sets
+  # read csv file and extract lines
+  f_handle = open("results/"+csv_fname, 'rU')
+  lines = [line.strip() for line in f_handle.readlines()]
+  header_str = lines[0]
+  f_handle.close()
+  # create stratified subsets. don't use the first line. It is the header
+  subsets = get_validationsets(lines[1:], folds)
+
+  # list to store the classifier accuracy for each fold
+  results = []
+  for index in range(len(subsets)):
     
+    validation_set = subsets[index]
+    training_set = subsets[:index] + subsets[index + 1:]
+    # flatten the training set into a list of stringss
+    training_set = [item for subset in training_set for item in subset]
+    
+    # output a csv file for each validation fold
+    test_fname = "validation/test_"+str(index)+csv_fname
+    out_f = open(test_fname, 'w')
+    out_f.write(header_str+"\n"+ string.join([(line+"\n") for line in training_set]))
+    out_f.close()
+
+    # make the classifier
+    classifier = Classifier(test_fname)
+    classifier.train()
+    # test each example in validation set
+    correct = 0
+    for line in validation_set:
+        if classifier.test_example(line.strip()):
+            correct += 1
+    accuracy = float(correct)*100/len(validation_set)
+    print "Accuracy: " + str(accuracy) +"%"
+    results.append(accuracy)
+    print "Overall accuracy: " + str(results.append(accuracy))
+  return sum(results)/folds
+              
+def get_validationsets(lines, folds):
+  """Returns a randomly stratified n-fold sample of a list of lines"""
+  spam_lines = []
+  nonspam_lines = []
+  # split files into spam or nonspam
+  for line in lines:
+    if 'nonspam' in line:
+      nonspam_lines.append(line)
+    else:
+      spam_lines.append(line)
+      
+  # make a random partition of spam
+  spam_partitions = random_partition(spam_lines, folds)
+  # make a random partition of nonspam
+  nonspam_partitions = random_partition(nonspam_lines, folds)
+  validation_sets = []
+  
+  # combining the two random partitions ensures the validation sets are
+  # properly stratified.
+  for i in xrange(folds):
+    validation_sets.append([])
+    validation_sets[i].extend(spam_partitions[i])
+    validation_sets[i].extend(nonspam_partitions[folds - i - 1])
+    random.shuffle(validation_sets[i])
+  return validation_sets
+  
 def random_partition(superset, k):
-    """Randomly partition superset into k subsets such that the lengths of no
-    two subsets differ in length by more than 1"""
+  """Randomly partition superset into k subsets such that the lengths of no
+  two subsets differ in length by more than 1"""
 
-    N = len(superset)
-    subsets = [[] for i in xrange(k)]
+  N = len(superset)
+  subsets = [[] for i in xrange(k)]
 
-    # main loop to assign every element in superset
-    for i in xrange(N):
-        for j in xrange(k):
-            if len(superset) == 0:
-                break
-            rand_index = random.randint(0, len(superset) - 1)
-            subsets[j].append(superset[rand_index])
-            superset.pop(rand_index)
-    return subsets
+  # main loop to assign every element in superset
+  for i in xrange(N):
+    # assign 1 element to each subset in turn
+    for j in xrange(k):
+      if len(superset) == 0:
+        break
+      rand_index = random.randint(0, len(superset) - 1)
+      subsets[j].append(superset[rand_index])
+      superset.pop(rand_index)
+  return subsets
